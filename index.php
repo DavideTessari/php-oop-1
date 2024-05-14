e38d7ff962571d7002a1328a7b991ecb3d273a47
DavideTessari <davtex@gmail.com>
2024-05-14 15:26:46 +0200

    add costruttore

--- index.php
+++ index.php
@@ -6,4 +6,11 @@ class Movie {
     public $director;
     public $year;
 
+     // Costruttore
+    public function __construct($title, $director, $year) {
+        $this->title = $title;
+        $this->director = $director;
+        $this->year = $year;
+    }
+
 ?>
